47c52a45e134454879baa93fe0248572b38af050
thufailalfath11 <thufailalfath11@gmail.com>
2024-11-18 15:28:23 +0700

    halo

--- src/components/Hero/subtext.tsx
+++
@@ -1,7 +0,0 @@
-import BlurIn2 from "../ui/blur-in2";
-
-export function Subtext() {
-  return (
-    <BlurIn2 word="Multimedia Application, Big Data, And Cyber Security." />
-  );
-}
--- src/components/Hero/text.tsx
+++
@@ -1,5 +0,0 @@
-import BlurIn from "@/components/ui/blur-in";
-
-export function BlurInDemo() {
-  return <BlurIn word="MBC" />;
-}
